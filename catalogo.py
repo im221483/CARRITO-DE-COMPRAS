e33a07c7d2be41fa30a70e2ab1d61344e54f42da
im221483 <im221483@itsatlixco.edu.mx>
2026-09-16 15:41:50 -0600

    Agrega funciones cargar Y mostrar catalogo

---
+++ catalogo.py
@@ -0,0 +1,12 @@
+def cargar_catalogo():
+    catalogo = {
+        "P001": {"nombre": "Café", "precio": 45.0, "stock": 20},
+        "P002": {"nombre": "Pan", "precio": 15.0, "stock": 30},
+        "P003": {"nombre": "Leche", "precio": 25.0, "stock": 15}
+    }
+    return catalogo
+
+def mostrar_catalogo(catalogo):
+    print("ID | Nombre | Precio | Stock")
+    for id_prod, datos in catalogo.items():
+        print(f"{id_prod} | {datos['nombre']} | {datos['precio']} | {datos['stock']}")
