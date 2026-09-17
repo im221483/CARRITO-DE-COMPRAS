d5594aa2045a966ffbfb2438dc112c419ea19b5a
im221483 <im221483@itsatlixco.edu.mx>
2026-09-16 18:41:24 -0600

    Agrega eliminar el producto

--- carrito.py
+++ carrito.py
@@ -2,3 +2,10 @@ def agregar_producto(carrito, catalogo, id_producto, cantidad):
     carrito.append((id_producto, cantidad))
 
 # Validar stock antes de agregar
+
+def eliminar_producto(carrito, id_producto):
+    for producto in carrito:
+        if producto[0] == id_producto:
+            carrito.remove(producto)
+            print("Producto eliminado.")
+            return
