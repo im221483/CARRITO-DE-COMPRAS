8b989823dd3cfa57be954faeab199fb3b606f281
im221483 <im221483@itsatlixco.edu.mx>
2026-09-16 23:21:10 -0600

    cambio en main para conflicto

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Versión main diferente
 from catalogo import cargar_catalogo, mostrar_catalogo
 from carrito import agregar_producto, eliminar_producto
 from descuentos import calcular_subtotal, aplicar_descuento
