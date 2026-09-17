91721ef8795964cbc20a5553b1405b5c021ae4a4
im221483 <im221483@itsatlixco.edu.mx>
2026-09-16 18:47:06 -0600

    Valida carrito vacio

--- carrito.py
+++ carrito.py
@@ -4,6 +4,9 @@ def agregar_producto(carrito, catalogo, id_producto, cantidad):
 # Validar stock antes de agregar
 
 def eliminar_producto(carrito, id_producto):
+        if not carrito:
+        print("El carrito esta vacio.")
+        return
     for producto in carrito:
         if producto[0] == id_producto:
             carrito.remove(producto)
